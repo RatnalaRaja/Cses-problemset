a53c87c8f6ec47c9db9a2f65870b198405c41637
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2022-10-14 05:29:33 +0530

    Add files via upload

---
+++ increasingarray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long n,sum=0;
+	cin>>n;
+	long long  arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=1;i<n;i++){
+		if(arr[i]<arr[i-1]){
+			sum+=abs(arr[i]-arr[i-1]);
+			arr[i]=arr[i-1];
+			
+		}
+
+	}
+	cout<<sum<<endl;
+	
+}
+
+
