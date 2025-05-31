652f90fe73f6462a9021370b72f69c72c5fc1acd
Ratnala Raja <97797913+RatnalaRaja@users.noreply.github.com>
2025-05-31 23:53:58 +0530

    Update increasingarray.cpp

--- increasingarray.cpp
+++ increasingarray.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main(){
 	long long n,sum=0;
-	cin>>n;
+	cin>> n;
 	long long  arr[n];
 	for(int i=0;i<n;i++){
 		cin>>arr[i];
 	}
-	for(int i=1;i<n;i++){
+	for(int i=1;i< n;i++){
 		if(arr[i]<arr[i-1]){
 			sum+=abs(arr[i]-arr[i-1]);
 			arr[i]=arr[i-1];
